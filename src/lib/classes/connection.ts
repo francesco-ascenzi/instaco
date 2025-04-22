e621d9f3a0b18a64af009bb7e7c675519e3976ee
fra.ascenzi <theouter.guy@gmail.com>
2025-04-22 23:34:31 +0100

    Added index creation within useCollection method

--- src/lib/classes/connection.ts
+++ src/lib/classes/connection.ts
@@ -3,7 +3,7 @@
  * @fund https://www.paypal.com/donate/?hosted_button_id=QL4PRUX9K9Y6A
  * @license Apache 2.0 
 ================================================================================================ */
-import mongodb, { Collection, Db, MongoClient } from "mongodb";
+import mongodb, { BSON, Collection, Db, MongoClient } from "mongodb";
 
 export default class Connection {
   public mongoClient: MongoClient;
@@ -34,16 +34,29 @@ export default class Connection {
     if (!this.mongoClient) return;
     await this.mongoClient.close();
   }
-  
+
   /** Use a collection from the database
    * 
    * @param {string} dbName - Database name
    * @param {string} collectionName - Collection name
    * @returns {Collection | string} - Collection object or error message
    */
-  async useCollection(dbName: string, collectionName: string): Promise<Collection | string> {
-    if (!this.mongoClient) return "Database connection is lost, try again";
-    const choseDb: Db = this.mongoClient.db(dbName);
-    return choseDb.collection(collectionName);
+  async useCollection(dbName: string, collectionName: string, type: number = 0): Promise<Collection | string> {
+    try {
+      if (!this.mongoClient) throw new Error("Database connection is lost, try again");
+
+      // Choose the database and collection
+      const choseDb: Db = this.mongoClient.db(dbName);
+      const collection: Collection = choseDb.collection(collectionName);
+
+      // Check indexes and create them if they don't exist
+      const indexes: BSON.Document[] = await collection.indexes();
+      const indexExists = indexes.some(index => index.name === "user");
+      if (!indexExists) await collection.createIndex({ user: 1 }, { name: "user" });
+
+      return collection;
+    } catch (err: unknown) {
+      return String(err);
+    }
   }
 }
